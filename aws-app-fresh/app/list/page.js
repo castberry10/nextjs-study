8649891333ebea455029ff64c473194071036696
최원영 <sable@kangwon.ac.kr>
2023-09-12 14:34:15 +0000

    Link 사용 / css

--- aws-app-fresh/app/list/page.js
+++ aws-app-fresh/app/list/page.js
@@ -4,6 +4,16 @@ export default function List() {
     <div>
 		<h4  style={{color:'red', fontSize:'14px'}}>리스트</h4>
 		
+		  <div>
+			  <div className="food">
+			  <h4>상품 1</h4>
+			  </div>
+			  <div className="food">
+			  <h4>상품 2</h4>
+			  </div>
+			  
+		  </div>
+		
 	</div>
   )
 }
--- aws-app-fresh/app/page.js
+++ aws-app-fresh/app/page.js
@@ -1,3 +1,4 @@
+import Link from "next/link"
 import Image from 'next/image'
 import styles from './page.module.css'
 
@@ -5,6 +6,11 @@ export default function Home() {
 	let name = 'paasrk'
   return (
     <div>
+		  <div className="navbar">
+			  <Link href = "/">홈</Link>
+			  <Link href = "/list">list</Link>
+			  
+		  </div>
 		<h4 className="title" style={{color:'red', fontSize:'14px'}}>안녕</h4>
 		<p className="title-sub">by {name}</p>
 	</div>
