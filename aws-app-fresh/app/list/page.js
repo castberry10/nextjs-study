75a911ea30f47d577373ab56a812856f7a30aaa8
최원영 <sable@kangwon.ac.kr>
2023-09-17 06:46:18 +0000

    이미지 삽입 / 크기 조정 css (img tag)

--- aws-app-fresh/app/list/page.js
+++ aws-app-fresh/app/list/page.js
@@ -1,20 +1,21 @@
 export default function List() {
 	let 상품 = ['Tomatoes', 'Pasta', 'Coconut']
-	
 	let name = 'paasrk'
+	let array = [2,3,4]
+	array.map(()=>{})
   return (
     <div>
-		<h4  style={{color:'red', fontSize:'14px'}}>리스트</h4>
-		
-		  <div>
-			  <div className="food">
-			  <h4>상품 1</h4>
-			  </div>
-			  <div className="food">
-			  <h4>상품 2</h4>
-			  </div>
-			  
-		  </div>
+		<h4  className="title">상품목록</h4>
+		{
+		  상품.map((a,i)=>{
+			  return(
+			  	<div className="food" key={i}>
+					  <img src='/food0.png' className='food-img'></img>
+					<h4>{a} $40</h4>
+				</div>
+			  )
+		  })
+		}
 		
 	</div>
   )
